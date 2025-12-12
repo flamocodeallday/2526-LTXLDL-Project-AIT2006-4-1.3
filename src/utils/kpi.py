0a77ac18839e309b972584fe910c32653aa4ff6b
SpringTung <24022488@vnu.edu.vn>
2025-12-12 23:19:32 +0700

    12/12/2025

--- src/utils/kpi.py
+++ src/utils/kpi.py
@@ -1,12 +1,24 @@
 import pandas as pd
-import numpy as np
 
-def kpi(df: pd.DataFrame) -> dict:
+def kpi(df: pd.DataFrame, qa_flags: pd.DataFrame) -> dict:
     df_calc = df.copy()
+    qa_flags = qa_flags.copy()
 
+    # Define time bins
+    bin = [0, 4, 7, 10, 16, 19, 24]
+    labels = ['Early Morning', 'Morning','Morning Rush', 'Midday', 'Evening Rush', 'Late Night']  
+    
+    # Assign time bins based on pickup and dropoff hours
+    df_calc['pickup_bin'] = pd.cut(df_calc['tpep_pickup_datetime'].dt.hour, bins=bin, labels=labels, right=False)
+    df_calc['dropoff_bin'] = pd.cut(df_calc['tpep_dropoff_datetime'].dt.hour, bins=bin, labels=labels, right=False)
+
+    # Calculation functions for 50th percentiles
     def p50(x): return x.quantile(0.5)
+
+    # Convenience function for 95th percentile
     def p95(x): return x.quantile(0.95)
 
+    # Define aggregation rules
     agg_rules = {
         'duration_p50': ('trip_duration_minutes', p50),
         'duration_p95': ('trip_duration_minutes', p95),
@@ -15,12 +27,28 @@ def kpi(df: pd.DataFrame) -> dict:
         'distance_p95': ('trip_distance', p95),
         'avg_distance': ('trip_distance', 'mean'),
         'trips':        ('trip_distance', 'count'),
-        'revuenue_per_trip':     ('total_amount', lambda x: x.sum() / x.count()),
-        'revenue_per_mile':     ('total_amount', lambda x: x.sum() / df_calc.loc[x.index, 'trip_distance'].sum()),
-        'avg_pickup_per_hour': ('tpep_pickup_datetime', lambda x: x.count() / ((x.max() - x.min()).total_seconds() / 3600)),
-        'avg_dropoff_per_hour': ('tpep_dropoff_datetime', lambda x: x.count() / ((x.max() - x.min()).total_seconds() / 3600)),
+        'revuenue_per_trip': ('total_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_total_amount']].sum() /
+                                                        x[~qa_flags.loc[x.index, 'invalid_total_amount']].count() ),
+        'revenue_per_mile': ('total_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_total_amount']].sum() / 
+                                                       df_calc.loc[x[~qa_flags.loc[x.index, 'invalid_total_amount']].index, 'trip_distance'].sum())
     }
-    
+
+    # Create binary columns for each time bin
+    for lb in labels:
+        df_calc[f'pickup_bin_{lb}'] = (df_calc['pickup_bin'] == lb).astype(int)
+        df_calc[f'dropoff_bin_{lb}'] = (df_calc['dropoff_bin'] == lb).astype(int)
+
+    # Add aggregation rules for trips per hour in each time bin
+    for i, lb in enumerate(labels):
+        bin_hours = bin[i+1] - bin[i]
+
+        agg_rules[f'{lb}'] = (
+            'trip_distance',
+            lambda x, lb=lb, h=bin_hours: (
+                f"{round(df_calc.loc[x.index, f'pickup_bin_{lb}'].sum() / h, 2)} / {round(df_calc.loc[x.index, f'dropoff_bin_{lb}'].sum() / h, 2)}"
+            )
+        )
+
     # A. Daily
     df_daily = df_calc.groupby(pd.Grouper(key='tpep_pickup_datetime', freq='D')).agg(**agg_rules).reset_index()
 
@@ -36,14 +64,29 @@ def kpi(df: pd.DataFrame) -> dict:
         "Monthly": df_monthly,
     }
 
-    # D. Time Bin
-    bin = [0, 4, 7, 10, 16, 19]
-    labels = ['Early Morning', 'Morning Rush', 'Midday', 'Evening Rush', 'Late Night']  
-    df_calc['time_bin'] = pd.cut(df_calc['tpep_pickup_datetime'].dt.hour, bins=bin, labels=labels, right=False)
-
-    df_time_bin = df_calc.groupby('time_bin').agg(**agg_rules).reset_index()
-
-    for index, row in df_time_bin.iterrows():
-        results[row['time_bin']] = (pd.DataFrame(row).T.set_index('time_bin')).reset_index(drop=True)
-
     return results
+
+def index_100_by_day(df : pd.DataFrame, value_col):
+    df = df.copy()
+
+    # Get the base value from the first day
+    base_value = df.iloc[0][value_col]
+
+    # Calculate index for each day
+    df[f'index_100_by_day_by_{value_col}'] = df[value_col] / base_value * 100
+
+    return df
+
+def index_dow(df, value_col):
+    df = df.copy()
+
+    # Aggregate total value by day of the week
+    agg = df.groupby('pickup_day_of_week')[value_col].sum().reset_index(name = 'total_trips')
+
+    # Calculate average value across all days
+    avg_value = agg['total_trips'].sum() / 7
+
+    # Calculate index for each day of the week
+    agg[f'index_dow_{value_col}'] = agg['total_trips'] / avg_value * 100
+
+    return agg
