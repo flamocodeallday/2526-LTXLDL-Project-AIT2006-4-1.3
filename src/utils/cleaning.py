fixing cleaning & normalizing, adding more QA rules, updating 16/12

src/utils/cleaning.py
@@ -5,10 +5,13 @@ import pandas as pd
 """
 def clean(normalized: pd.DataFrame, qa_flags: pd.DataFrame, threshold: int = 5):
     # Rules to remove due to invalid values
-    remove = ['is_duplicate', 'invalid_time_order', 'invalid_duration', 'invalid_distance', 'invalid_speed', 'invalid_month']
+    remove = ['is_duplicate', 'missing_datetime', 'invalid_time_order', 'invalid_month', 'invalid_duration', 'invalid_distance', 'invalid_speed']
+
+    # Columns that is not in remove and used to count total violations
+    flag_keep = [col for col in qa_flags.columns if col not in remove]
 
     # Identify rows that violates more than threshold columns
-    qa_flags['total_violations'] = qa_flags.sum(axis=1)
+    qa_flags['total_violations'] = qa_flags[flag_keep].sum(axis=1)
     qa_flags['is_garbage_row'] = qa_flags['total_violations'] > threshold
     remove.append('is_garbage_row')
 

src/utils/normalizing.py
@@ -13,7 +13,7 @@ Columns after: ['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_coun
                 'payment_type', 'fare_amount', 'extra', 'tip_amount', 'tolls_amount', 
                 'total_amount', 'congestion_surcharge', 'ratecodeID_name', 'payment_type_name', 
                 'trip_duration_seconds', 'trip_duration_minutes', 'avg_speed_mph', 
-                'PU_Borough', 'PU_Zone', 'DO_Borough', 'DO_Zone', 'pickup_day_of_week']
+                'PU_Borough', 'PU_Zone', 'DO_Borough', 'DO_Zone', 'pickup_day_of_week', 'is_weekend', 'computed_total_amount']
 
 """
 
@@ -28,6 +28,9 @@ ratecodeID_map = {1: 'Standard rate', 2: 'JFK', 3: 'Newark', 4: 'Nassau or Westc
 def normalize(df_raw: pd.DataFrame) -> pd.DataFrame:
     df = df_raw.copy()
 
+    # Normalize datetime columns
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'], errors='coerce').dt.tz_localize('America/New_York', ambiguous='NaT', nonexistent='NaT')
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'], errors='coerce').dt.tz_localize('America/New_York', ambiguous='NaT', nonexistent='NaT')
     # Normalize RatecodeID 
     df['ratecodeID_name'] = df['RatecodeID'].map(ratecodeID_map)
     # Normalize Payment type
@@ -57,8 +60,11 @@ def normalize(df_raw: pd.DataFrame) -> pd.DataFrame:
     # Create new feature: trip's average speed
     df['avg_speed_mph'] = round(df['trip_distance'] / (df['trip_duration_seconds'] / 3600), 2)
     df['avg_speed_mph'].replace([np.inf, -np.inf], np.nan, inplace=True)
-    # Create new feture: trip's day in week (based on pick up time)
+    # Create new feture: trip's day in week, is_weekend (based on pick up time)
     df['pickup_day_of_week'] = df['tpep_pickup_datetime'].dt.day_name()
+    df['is_weekend'] = df['pickup_day_of_week'].isin(['Saturday', 'Sunday'])
+    #Create new feature: Compute sum of known components (use 0 for missing) to compare with total_amount
+    df['computed_total_amount'] = df[['fare_amount', 'tolls_amount', 'tip_amount', 'extra', 'congestion_surcharge', 'mta_tax', 'improvement_surcharge']].fillna(0).sum(axis=1)
 
     # Remove columns due to the number of NA values and based on the purpose of analysing
     cols_to_drop = [

src/utils/qa_rules.py
@@ -7,52 +7,82 @@ import numpy as np
 '''
 def run_quality_check(df: pd.DataFrame, current_month: int) -> pd.DataFrame:
     qa_flags = pd.DataFrame(index=df.index)
-
+    
     # Rule 1: Duplicated rows -> Action: Exclude
     qa_flags['is_duplicate'] = df.duplicated()
 
-    # Rule 2: drop off before pick up -> Action: Exclude
+    # RULES RELATED TO DATETIME AND TRIP LOGIC
+    # Rule 2: Missing pickup or dropoff datetime -> Action: Exclude
+    qa_flags['missing_datetime'] = df['tpep_pickup_datetime'].isna() | df['tpep_dropoff_datetime'].isna()
+
+    # Rule 3: Drop off before pick up -> Action: Exclude
     qa_flags['invalid_time_order'] = df["tpep_dropoff_datetime"] < df["tpep_pickup_datetime"]
 
-    # Rule 3: Duration is negative -> Action: Exclude
-    qa_flags['invalid_duration'] = df['trip_duration_minutes'] <= 0
-
-    # Rule 4: Distance is negative -> Action: Exclude 
-    qa_flags['invalid_distance'] = df["trip_distance"] <= 0
-
-    # Rule 5: Speed is negative -> Action: Exclude
-    qa_flags['invalid_speed'] = df['avg_speed_mph'] <= 0
-
-    # Rule 6: Invalid month, year -> Action: Exclude 
+    # Rule 4: Invalid month, year -> Action: Exclude 
     qa_flags['invalid_month'] = (df['tpep_pickup_datetime'].dt.month != current_month) | (df['tpep_pickup_datetime'].dt.year != 2021)
 
-    # Rule 7: Negative fare amount -> Action: Flag
+    # RULES RELATED TO TRIP FEATURES
+    # Rule 5: Duration is negative -> Action: Exclude
+    qa_flags['invalid_duration'] = df['trip_duration_minutes'] <= 0
+
+    # Rule 6: Distance is negative -> Action: Exclude 
+    qa_flags['invalid_distance'] = df["trip_distance"] <= 0
+
+    # Rule 7: Speed is negative -> Action: Exclude
+    qa_flags['invalid_speed'] = df['avg_speed_mph'] <= 0
+
+    # Rule 8: Zero fare but distance > 0 -> Action: Flag (suspicious)
+    qa_flags['suspicious_zero_fare'] = (df['fare_amount'] == 0) & (df['trip_distance'] > 0)
+
+    # Rule 9: Very short duration but nontrivial distance -> Action: Flag
+    qa_flags['short_duration_long_distance'] = (df['trip_duration_minutes'] < 1) & (df['trip_distance'] > 1)
+
+    # Rule 10: Excessive average speed -> Action: Flag
+    qa_flags['excessive_speed'] = df['avg_speed_mph'] > 66
+
+    # Rule 11: Excessive duration (more than 24 hours) -> Action: Flag
+    qa_flags['excessive_duration'] = df['trip_duration_minutes'] > (24 * 60)
+
+    # RULES RELATED TO PAYMENT AND AMOUNTS
+    # Rule 12: Negative fare amount -> Action: Flag
     qa_flags['invalid_fare_amount'] = df['fare_amount'] <= 0
 
-    # Rule 8: Negative tip amount -> Action: Flag
+    # Rule 13: Negative tip amount -> Action: Flag
     qa_flags['invalid_tip_amount'] = df['tip_amount'] < 0
+    
+    # Rule 14: Negative extra amount -> Action: Flag
+    qa_flags['invalid_extra'] = df['extra'] < 0
 
-    # Rule 9: Negative total amount -> Action: Flag
+    # Rule 15: Negative tolls amount -> Action: Flag
+    qa_flags['invalid_tolls_amount'] = df['tolls_amount'] < 0
+
+    # Rule 16: Negative total amount -> Action: Flag
     qa_flags['invalid_total_amount'] = df['total_amount'] <= 0
 
-    # Rule 10: Invalid payment type (not in range [0,6]) -> Action: Flag
+    # Rule 17: Fare/amount arithmetic mismatch -> Action: Flag
+    qa_flags['fare_total_mismatch'] = (df['total_amount'].fillna(0) - df['computed_total_amount']).abs() > 1.0
+
+    # Rule 18: Invalid payment type (not in range [0,6]) -> Action: Flag
     qa_flags['invalid_payment_type'] = (df['payment_type'] < 0) | (df['payment_type'] > 6)
 
-    # Rule 11: Invalid passenger counts -> Action: Flag
-    qa_flags['invalid_passenger_count'] = df['passenger_count'] == 0
+    # Rule 19: Invalid RatecodeID -> Action: Flag
+    qa_flags['invalid_ratecode'] = ~df['RatecodeID'].isin([1,2,3,4,5,6,99])
 
-    # Rule 12: Zone ID does not exist -> Action: Flag
+    # Rule 20: Unusual passenger counts -> Action: Flag
+    qa_flags['unusual_passenger_count'] = (df['passenger_count'] == 0 ) | (df['passenger_count'] > 5)
+
+    # Rule 21: Zone ID does not exist -> Action: Flag
     qa_flags['invalid_zone'] = df[['PU_Borough', 'PU_Zone', 'DO_Borough', 'DO_Zone']].isna().any(axis = 1)
 
     return qa_flags
 
 '''
     This function summarizes qa_flags into a pandas Series (format: "count/pct%").
-    - The first 11 entries summarize violations for each individual rule.
-    - The 12th entry (Total) summarizes the count of *unique trips* (rows) 
+    - The first 22 entries summarize violations for each individual rule.
+    - The 23th entry (Total) summarizes the count of *unique trips* (rows) 
     that violated *at least one* rule.
 '''
-def summarize_qa_flags(qa_flags: pd.DataFrame) -> pd.Series:
+def summarize_qa_flags(qa_flags: pd.DataFrame):
 
     total_records = len(qa_flags)
     
@@ -75,4 +105,8 @@ def summarize_qa_flags(qa_flags: pd.DataFrame) -> pd.Series:
     total_string = f"{total_violated_rows}/{total_violated_percent}%"
     
     final_list.append(total_string)
-    return pd.Series(final_list)
+
+    # Calulate threshold for garbage rows
+    qa_flags['total_violations'] = qa_flags.sum(axis=1)
+    threshold = qa_flags['total_violations'].quantile(0.95).astype(int)
+    return pd.Series(final_list), threshold