dac361fe7516e2f30e4996917c1abea5ccb01996
SpringTung <24022488@vnu.edu.vn>
2025-12-18 22:38:28 +0700

    18/12/2025

--- src/utils/kpi.py
+++ src/utils/kpi.py
@@ -1,39 +1,71 @@
 import pandas as pd
 
-def kpi(df: pd.DataFrame, qa_flags: pd.DataFrame) -> dict:
-    df_calc = df.copy()
-    qa_flags = qa_flags.copy()
+def aggregate_kpis(df_month: pd.DataFrame, qa_flags: pd.DataFrame) -> dict:
+    df_calc = df_month.copy()
 
     # Define time bins
     bin = [0, 4, 7, 10, 16, 19, 24]
-    labels = ['Early Morning', 'Morning','Morning Rush', 'Midday', 'Evening Rush', 'Late Night']  
-    
+    labels = ['Early Morning', 'Morning','Morning Rush', 'Midday', 'Evening Rush', 'Late Night']
+
     # Assign time bins based on pickup and dropoff hours
     df_calc['pickup_bin'] = pd.cut(df_calc['tpep_pickup_datetime'].dt.hour, bins=bin, labels=labels, right=False)
     df_calc['dropoff_bin'] = pd.cut(df_calc['tpep_dropoff_datetime'].dt.hour, bins=bin, labels=labels, right=False)
 
-    # Calculation functions for 50th percentiles
-    def p50(x): return x.quantile(0.5)
-
-    # Convenience function for 95th percentile
-    def p95(x): return x.quantile(0.95)
-
     # Define aggregation rules
     agg_rules = {
-        'Date': ('tpep_pickup_datetime', lambda x: x.dt.date.iloc[0]),
+        'Date': ('tpep_pickup_datetime', lambda x: x.dt.date.iloc[-1]),
         'Day_of_Week': ('pickup_day_of_week', 'first'),
         'Total_trips': ('tpep_pickup_datetime', 'count'),
-        'Total_fare': ('fare_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_fare_amount']].sum()),
-        'Total amount': ('total_amount', lambda x: x[(~qa_flags.loc[x.index, 'invalid_total_amount']) & (~qa_flags.loc[x.index, 'fare_total_mismatch'])].sum()),
-        'duration_p50': ('trip_duration_minutes', lambda x: p50(x[(~qa_flags.loc[x.index, 'excessive_duration']) & (~qa_flags.loc[x.index, 'short_duration_long_distance'])])),
-        'duration_p95': ('trip_duration_minutes', lambda x: p95(x[(~qa_flags.loc[x.index, 'excessive_duration']) & (~qa_flags.loc[x.index, 'short_duration_long_distance'])])),
-        'speed_p50': ('avg_speed_mph', lambda x: p50(x[~qa_flags.loc[x.index, 'excessive_speed']])),
-        'distance_p50': ('trip_distance', lambda x: p50(x[(~qa_flags.loc[x.index, 'suspicious_zero_fare']) & (~qa_flags.loc[x.index, 'short_duration_long_distance'])])),
-        'distance_p95': ('trip_distance', lambda x: p95(x[(~qa_flags.loc[x.index, 'suspicious_zero_fare']) & (~qa_flags.loc[x.index, 'short_duration_long_distance'])])),
-        'avg_distance': ('trip_distance', lambda x: x[(~qa_flags.loc[x.index, 'suspicious_zero_fare']) & (~qa_flags.loc[x.index, 'short_duration_long_distance'])].mean()),
-        'trips': ('trip_distance', 'count'),
-        'revenue_per_trip': ('fare_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_fare_amount']].sum() / x[~qa_flags.loc[x.index, 'invalid_fare_amount']].count()),
-        'revenue_per_mile': ('fare_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_fare_amount']].sum() / df_calc.loc[x[~qa_flags.loc[x.index, 'invalid_fare_amount']].index, 'trip_distance'].sum())
+        'Total_fare': (
+            'fare_amount', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'invalid_fare_amount']) & 
+                (~qa_flags.loc[x.index, 'suspicious_zero_fare'])
+                ].sum()),
+
+        'Total_amount': ('total_amount', lambda x: x[(~qa_flags.loc[x.index, 'invalid_total_amount']) & (~qa_flags.loc[x.index, 'fare_total_mismatch'])].sum()),
+        
+        'speed_p50':    ('avg_speed_mph', lambda x: x[~qa_flags.loc[x.index, 'excessive_speed']].quantile(0.5)),
+
+        'duration_p50': (
+            'trip_duration_minutes', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'excessive_duration']) & 
+                (~qa_flags.loc[x.index, 'short_duration_long_distance'])
+            ].quantile(0.5)),
+        
+        'duration_p95': (
+            'trip_duration_minutes', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'excessive_duration']) & 
+                (~qa_flags.loc[x.index, 'short_duration_long_distance'])
+            ].quantile(0.95)),
+        
+        'distance_p50': (
+            'trip_distance', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'suspicious_zero_fare']) & 
+                (~qa_flags.loc[x.index, 'short_duration_long_distance'])
+            ].quantile(0.5)),
+        
+        'distance_p95': (
+            'trip_distance', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'suspicious_zero_fare']) & 
+                (~qa_flags.loc[x.index, 'short_duration_long_distance'])
+            ].quantile(0.95)),
+        
+        'avg_distance': (
+            'trip_distance', 
+            lambda x: x[
+                (~qa_flags.loc[x.index, 'suspicious_zero_fare']) & 
+                (~qa_flags.loc[x.index, 'short_duration_long_distance'])
+            ].mean()),
+        
+        'revenue_per_trip': ('fare_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_fare_amount']].sum() /
+                                                        x[~qa_flags.loc[x.index, 'invalid_fare_amount']].count() ),
+        'revenue_per_mile': ('fare_amount', lambda x: x[~qa_flags.loc[x.index, 'invalid_fare_amount']].sum() / 
+                                                       df_calc.loc[x[~qa_flags.loc[x.index, 'invalid_fare_amount']].index, 'trip_distance'].sum())
     }
 
     # Create binary columns for each time bin
@@ -55,6 +87,15 @@ def kpi(df: pd.DataFrame, qa_flags: pd.DataFrame) -> dict:
     # A. Daily
     df_daily = df_calc.groupby(pd.Grouper(key='tpep_pickup_datetime', freq='D')).agg(**agg_rules).reset_index()
 
+    base_value = df_daily.loc[0, 'Total_trips']
+
+    if base_value == 0:
+        base_value = 1  # To avoid division by zero
+
+    df_daily[f'index_100_by_day_by_trips'] = (
+        df_daily['Total_trips'] / base_value * 100
+    )
+
     # B. Weekly
     df_weekly = df_calc.groupby(pd.Grouper(key='tpep_pickup_datetime', freq='W')).agg(**agg_rules).reset_index()
 
@@ -68,28 +109,3 @@ def kpi(df: pd.DataFrame, qa_flags: pd.DataFrame) -> dict:
     }
 
     return results
-
-def index_100_by_day(df : pd.DataFrame, value_col):
-    df = df.copy()
-
-    # Get the base value from the first day
-    base_value = df.iloc[0][value_col]
-
-    # Calculate index for each day
-    df[f'index_100_by_day_by_{value_col}'] = df[value_col] / base_value * 100
-
-    return df
-
-def index_dow(df, value_col):
-    df = df.copy()
-
-    # Aggregate total value by day of the week
-    agg = df.groupby('pickup_day_of_week')[value_col].sum().reset_index(name = 'total_trips')
-
-    # Calculate average value across all days
-    avg_value = agg['total_trips'].sum() / 7
-
-    # Calculate index for each day of the week
-    agg[f'index_dow_{value_col}'] = agg['total_trips'] / avg_value * 100
-
-    return agg
